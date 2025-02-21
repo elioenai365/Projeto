06c1a2bf7792ba29c62ea706031a54277e315016
elioenai365 <elioenaicoutinho@hotmail.com>
2025-02-20 22:32:14 -0300

    Adicionar bibliotecas

--- Projeto.c
+++ Projeto.c
@@ -1,5 +1,10 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+// Bibliotecas d prejeto
+#include <stdio.h> // Padrao de entrada e saida
+#include "pico/stdlib.h"// Entrada e saida do pico
+#include "hardware/i2c.h"// biblioteca i²c
+#include "hardware/adc.h"// Analogico em Digital
+#include "ssd1306.h"// Bibliteca do visor
+
 
 
 
