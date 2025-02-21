4750e12878b2485fdbb59f3c20c4c96f6a22d761
elioenai365 <elioenaicoutinho@hotmail.com>
2025-02-20 23:33:57 -0300

    resolver cnflito draw rect

--- lib/ssd1306.h
+++ lib/ssd1306.h
@@ -48,3 +48,4 @@ void ssd1306_vline(ssd1306_t *ssd, uint8_t x, uint8_t y0, uint8_t y1, bool value
 void ssd1306_draw_char(ssd1306_t *ssd, char c, uint8_t x, uint8_t y);
 void ssd1306_draw_string(ssd1306_t *ssd, const char *str, uint8_t x, uint8_t y);
 void ssd1306_clear(ssd1306_t *ssd);
+void ssd1306_draw_rect(ssd1306_t *ssd, int x, int y, int width, int height, bool fill);
